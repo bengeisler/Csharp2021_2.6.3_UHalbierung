1961ed601ecafb2b6b2867bc3dce7e0c342401e4
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-02-05 14:13:48 +0100

    New

---
+++ UHalbierung/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace UHalbierung
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnAnzeigen = new System.Windows.Forms.Button();
+			this.lblAusgabe = new System.Windows.Forms.Label();
+			this.txtEingabe = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// btnAnzeigen
+			// 
+			this.btnAnzeigen.Location = new System.Drawing.Point(311, 43);
+			this.btnAnzeigen.Name = "btnAnzeigen";
+			this.btnAnzeigen.Size = new System.Drawing.Size(75, 23);
+			this.btnAnzeigen.TabIndex = 0;
+			this.btnAnzeigen.Text = "Anzeigen";
+			this.btnAnzeigen.UseVisualStyleBackColor = true;
+			this.btnAnzeigen.Click += new System.EventHandler(this.btnAnzeigen_Click);
+			// 
+			// lblAusgabe
+			// 
+			this.lblAusgabe.AutoSize = true;
+			this.lblAusgabe.Location = new System.Drawing.Point(12, 11);
+			this.lblAusgabe.Name = "lblAusgabe";
+			this.lblAusgabe.Size = new System.Drawing.Size(35, 13);
+			this.lblAusgabe.TabIndex = 1;
+			this.lblAusgabe.Text = "label1";
+			// 
+			// txtEingabe
+			// 
+			this.txtEingabe.Location = new System.Drawing.Point(311, 8);
+			this.txtEingabe.Name = "txtEingabe";
+			this.txtEingabe.Size = new System.Drawing.Size(75, 20);
+			this.txtEingabe.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(398, 311);
+			this.Controls.Add(this.txtEingabe);
+			this.Controls.Add(this.lblAusgabe);
+			this.Controls.Add(this.btnAnzeigen);
+			this.Name = "Form1";
+			this.Text = "Halbierung";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnAnzeigen;
+		private System.Windows.Forms.Label lblAusgabe;
+		private System.Windows.Forms.TextBox txtEingabe;
+	}
+}
+
---
+++ UHalbierung/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UHalbierung
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnAnzeigen_Click(object sender, EventArgs e)
+		{
+			// Text aus Textfeld einlesen, konvertieren, in einer
+			// Variablen eines passenden Datentyps speichern
+			double eingabe = Convert.ToDouble(txtEingabe.Text);
+
+			// Bestehenden Text des Labels löschen
+			lblAusgabe.Text = "";
+
+			// Passende Schleife wählen, Schleifenbedingung wählen
+			while (eingabe > 0.001)
+			{
+				// Division durchführen
+				eingabe = eingabe / 2;
+
+				// Ergebnis ausgeben, Zeilenumbruch  nicht vergessen
+				lblAusgabe.Text += (eingabe / 2) + "\n";
+			}
+		}
+	}
+}
